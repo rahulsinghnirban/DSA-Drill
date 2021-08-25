8257d08362df5069bcce8055740e27e018c746d6
rahulsinghnirban <80592933+rahulsinghnirban@users.noreply.github.com>
2021-08-26 00:12:56 +0530

    Create MaximumSumNonAdjacentElements(Pepcoding).java

---
+++ Dynamic Programming/MaximumSumNonAdjacentElements(Pepcoding).java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int inc = arr[0];
+        int exc = 0;
+        
+        for(int i=1; i<arr.length; i++){
+            int ninc = exc + arr[i];
+            int nexc = Math.max(inc, exc);
+            
+            inc = ninc;
+            exc = nexc;
+        }
+        
+        System.out.println(Math.max(inc, exc));
+
+    }
+}
