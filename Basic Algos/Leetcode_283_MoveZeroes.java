0499b931790193865a9907f10c0bdf86692511c9
rahulsinghnirban <80592933+rahulsinghnirban@users.noreply.github.com>
2021-08-29 00:33:43 +0530

    Create Leetcode_283_MoveZeroes.java

---
+++ Basic Algos/Leetcode_283_MoveZeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int index = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            
+            if(nums[i] != 0){
+                nums[index] = nums[i];
+                index++;
+            }
+
+        }
+        
+        while(index!=nums.length){
+            nums[index++] = 0;
+        }
+    }
+}
