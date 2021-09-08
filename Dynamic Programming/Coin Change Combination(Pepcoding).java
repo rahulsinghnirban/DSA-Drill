457fed0fe3daff771f4501835c600d3490fc65aa
rahulsinghnirban <80592933+rahulsinghnirban@users.noreply.github.com>
2021-09-08 23:11:56 +0530

    Create Coin Change Combination(Pepcoding).java

---
+++ Dynamic Programming/Coin Change Combination(Pepcoding).java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int amt = sc.nextInt();
+        
+        int[] dp = new int[amt+1];
+        
+        dp[0] = 1;
+        
+        for(int i=0; i<arr.length; i++){
+            
+            for(int j=arr[i]; j<dp.length; j++){
+                
+                dp[j] += dp[j-arr[i]];
+                
+            }
+            
+        }
+        
+        System.out.println(dp[amt]);
+        
+    }
+}
